74453f4e5b38a78aafa0efb1d23341cbf37125ca
Kacper Stankiewicz <104934125+kacperstank@users.noreply.github.com>
2024-12-16 23:37:28 +0100

    Initial API folder

---
+++ backend/task_manager_api/urls.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from django.urls import path, include
+urlpatterns = [
+    path('admin/', admin.site.urls),  # Admin interface
+    path('', include('tasks.urls')),  # Include app-specific URLs
+]
